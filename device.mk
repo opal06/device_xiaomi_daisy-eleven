fafd932fa5dadf9e2370a9f6650e64a40db7ec0f
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-09-07 12:32:08 +0300

    daisy: Downrev perf to 2.0
    * Perf 2.2 seems to have quite a few issues with the new AIDL power HAL
    
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -354,10 +354,7 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Power
 PRODUCT_PACKAGES += \
